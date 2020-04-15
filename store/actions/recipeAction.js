bc4070687c10eff03ca65ad68c471a436489b38e
vinwik <vinwik@hotmail.com>
2020-04-15 05:19:39 +0100

    GET_RECIPE_FROM_FAVOURITES + SET_LOADING added

--- store/actions/recipeAction.js
+++ store/actions/recipeAction.js
@@ -1,11 +1,11 @@
-import { GET_RECIPE } from "./types";
+import { GET_RECIPE, GET_RECIPE_FROM_FAVOURITES, SET_LOADING } from "./types";
 import { AsyncStorage } from "react-native";
 
 import { API_KEY } from "../../env";
 
 export const getRecipe = (recipeId) => async (dispatch) => {
   dispatch({
-    type: "SET_LOADING",
+    type: SET_LOADING,
   });
 
   const value = await AsyncStorage.getItem(recipeId);
@@ -33,6 +33,40 @@ export const getRecipe = (recipeId) => async (dispatch) => {
 
     dispatch({
       type: GET_RECIPE,
+      payload: transformedData,
+    });
+  }
+};
+export const getRecipeFromFavourites = (recipeId) => async (dispatch) => {
+  dispatch({
+    type: SET_LOADING,
+  });
+
+  const value = await AsyncStorage.getItem(recipeId);
+  const parsedRecipe = JSON.parse(value);
+
+  if (parsedRecipe !== null) {
+    dispatch({
+      type: GET_RECIPE_FROM_FAVOURITES,
+      payload: parsedRecipe,
+    });
+  } else {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
+    );
+    const data = await response.json();
+
+    const transformedData = {
+      ...data,
+      steps: data.analyzedInstructions.length
+        ? data.analyzedInstructions[0].steps
+        : [],
+    };
+
+    await AsyncStorage.setItem(recipeId, JSON.stringify(transformedData));
+
+    dispatch({
+      type: GET_RECIPE_FROM_FAVOURITES,
       payload: transformedData,
     });
   }
