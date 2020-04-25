fbdcd60b67291ab9b152a157e4be70f8cec6baae
vinwik <vinwik@hotmail.com>
2020-04-25 21:56:05 +0100

    Added no recipe found

--- components/RecipeCardList.js
+++ components/RecipeCardList.js
@@ -6,7 +6,9 @@ import {
   FlatList,
   ScrollView,
   ActivityIndicator,
+  Dimensions,
 } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import RecipeCard from "./RecipeCard";
 import { useSelector, useDispatch } from "react-redux";
 import { FadeIn } from "../animations/FadeIn";
@@ -14,29 +16,73 @@ import { FadeIn } from "../animations/FadeIn";
 const RecipeCardList = (props) => {
   const recipes = useSelector((state) => state.recipe.recipes);
   const isLoading = useSelector((state) => state.recipe.loading);
+  const searchValue = useSelector((state) => state.recipe.searchValue);
   const scroll = useRef();
 
   useEffect(() => {
-    scroll.current.scrollToOffset({ animated: true, offset: 0 });
+    recipes.length &&
+      scroll.current.scrollToOffset({ animated: true, offset: 0 });
   }, [recipes]);
 
   return (
     <View style={styles.cardListContainer}>
-      {/* {isLoading && !recipes ? (
-        <ActivityIndicator />
-      ) : ( */}
-      <FlatList
-        horizontal
-        ref={scroll}
-        showsHorizontalScrollIndicator={false}
-        style={styles.cardList}
-        data={recipes}
-        renderItem={({ item, index }) => (
-          <RecipeCard recipe={item} i={index} length={recipes.length} />
-        )}
-        keyExtractor={(item) => item.id.toString()}
-      />
-      {/* )} */}
+      {!isLoading && !recipes.length ? (
+        // <ActivityIndicator />
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            top: 20,
+          }}
+        >
+          <Text
+            style={{
+              color: "#888",
+              fontSize: 18,
+            }}
+          >{`No result found for :`}</Text>
+          <Text
+            style={{
+              color: "#888",
+              fontSize: 18,
+              fontStyle: "italic",
+            }}
+          >{`"${searchValue}"`}</Text>
+          <AntDesign
+            name="warning"
+            size={100}
+            color={"#cecece"}
+            style={{ top: (Dimensions.get("screen").height - 98) / 4 - 50 }}
+          />
+          {/* <Text
+            style={{
+              color: "#888",
+              fontSize: 18,
+              top: 80,
+            }}
+          >{`No result found for :`}</Text>
+          <Text
+            style={{
+              color: "#888",
+              fontSize: 18,
+              fontStyle: "italic",
+              top: 80,
+            }}
+          >{`"${search}"`}</Text> */}
+        </View>
+      ) : (
+        <FlatList
+          horizontal
+          ref={scroll}
+          showsHorizontalScrollIndicator={false}
+          style={styles.cardList}
+          data={recipes}
+          renderItem={({ item, index }) => (
+            <RecipeCard recipe={item} i={index} length={recipes.length} />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
     </View>
   );
 };
@@ -45,7 +91,7 @@ export default RecipeCardList;
 
 const styles = StyleSheet.create({
   cardListContainer: {
-    // flexDirection: "row",
+    flexDirection: "row",
     // height: "40%",
     // height: "100%",
     // width: "100%",
