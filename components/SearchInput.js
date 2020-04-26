ca55913e28af402c4757777666aebcad9675edfc
vinwik <vinwik@hotmail.com>
2020-04-26 11:50:27 +0100

    Autocomplete recipe search

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Keyboard,
+} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { FadeIn } from "../animations/FadeIn";
+import { API_KEY } from "../env";
 
 const SearchInput = (props) => {
   const [search, setSearch] = useState("");
+  const [autocompleteSearch, setAutocompleteSearch] = useState([]);
 
   const inputHandler = (enteredSearch) => {
     props.setSearch(enteredSearch);
+    searchAutocomplete(enteredSearch);
+  };
+
+  const autocompleteHandler = (autocomplete) => {
+    props.setSearch(autocomplete);
+    props.searchHandler();
+    setAutocompleteSearch([]);
+    Keyboard.dismiss();
+  };
+
+  const submitHandler = () => {
+    props.searchHandler();
+    setAutocompleteSearch([]);
   };
 
   const searchHandler = () => {
@@ -19,32 +41,61 @@ const SearchInput = (props) => {
     }
   };
 
+  const searchAutocomplete = async (search) => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/autocomplete?apiKey=${API_KEY}&number=6&query=${search}`
+    );
+
+    const data = await response.json();
+    // console.log(data);
+    setAutocompleteSearch(data);
+  };
+
   return (
-    <View style={styles.inputContainer}>
-      <AntDesign
-        name="search1"
-        size={20}
-        color="#8e8e8e"
-        style={styles.searchIcon}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder={props.placeHolder}
-        onChangeText={inputHandler}
-        value={props.search}
-        onSubmitEditing={props.searchHandler}
-      />
-      {props.search ? (
-        <FadeIn duration={200}>
-          <AntDesign
-            name="close"
-            size={20}
-            color="#8e8e8e"
-            style={styles.closeIcon}
-            onPress={() => props.setSearch("")}
-          />
-        </FadeIn>
-      ) : null}
+    <View style={styles.searchBarContainer}>
+      <View
+        style={[
+          styles.inputContainer,
+          autocompleteSearch.length && styles.borderBottom,
+        ]}
+      >
+        <AntDesign
+          name="search1"
+          size={20}
+          color="#8e8e8e"
+          style={styles.searchIcon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={props.placeHolder}
+          onChangeText={inputHandler}
+          value={props.search}
+          onSubmitEditing={() => submitHandler()}
+        />
+        {props.search ? (
+          <FadeIn duration={200}>
+            <AntDesign
+              name="close"
+              size={20}
+              color="#8e8e8e"
+              style={styles.closeIcon}
+              onPress={() => props.setSearch("")}
+            />
+          </FadeIn>
+        ) : null}
+      </View>
+      {autocompleteSearch &&
+        autocompleteSearch.map((item) => {
+          return (
+            <TouchableHighlight
+              key={item.id}
+              underlayColor="#f2f2f2"
+              onPress={() => autocompleteHandler(item.title)}
+            >
+              <Text style={styles.autocompleteItem}>{item.title}</Text>
+            </TouchableHighlight>
+          );
+        })}
     </View>
   );
 };
@@ -52,7 +103,7 @@ const SearchInput = (props) => {
 export default SearchInput;
 
 const styles = StyleSheet.create({
-  inputContainer: {
+  searchBarContainer: {
     backgroundColor: "#fff",
     width: "90%",
     borderRadius: 25,
@@ -60,10 +111,29 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 20,
     elevation: 5,
+    // flexDirection: "row",
+    // alignItems: "center",
+    // justifyContent: "space-between",
+    zIndex: 10,
+    overflow: "hidden",
+  },
+  inputContainer: {
+    backgroundColor: "#fff",
+    // width: "90%",
+    borderRadius: 25,
+    // alignSelf: "center",
+    // position: "absolute",
+    // top: 20,
+    // elevation: 5,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    zIndex: 10,
+
+    // zIndex: 10,
+  },
+  borderBottom: {
+    borderBottomWidth: 0.4,
+    borderBottomColor: "#8e8e8e",
   },
   searchIcon: {
     paddingLeft: 15,
@@ -76,5 +146,14 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     // width: "100%",
     flexGrow: 1,
+  },
+  autocompleteItem: {
+    paddingVertical: 15,
+    paddingLeft: 20,
+    color: "#666",
+    // width: "100%",
+    // flexGrow: 1,
+    // borderBottomWidth: 1,
+    // borderBottomColor: "#888",
   },
 });
