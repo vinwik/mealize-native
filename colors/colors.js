0b1738387fc888720ed77dbfe0b8a82aff71c258
Vincent Wikart <vinwik@vincents-mbp.lan>
2020-05-26 02:22:37 +0100

    Functional searchbar in header

--- colors/colors.js
+++ colors/colors.js
@@ -2,6 +2,7 @@ export const colors = {
   green: "#2ca52c",
   darkGreen: "#036a03",
   paleGreen: "#4CAF50",
+  paleGreenSearch: "#2D9434",
   paleDarkGreen: "#15771b",
   red: "#ce3737",
   paleRed: "#de6461",
